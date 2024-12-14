70c22d3c7332aca062dd2821ad308e4c1727411f
YWYH <137610166+oYwyh@users.noreply.github.com>
2024-12-14 22:00:49 +0200

    Delete yazi directory

--- yazi/scripts/reveal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -d "$1" ]; then
-    # If it's a directory, open it directly
-    dolphin "$1"
-else
-    # If it's a file, open the containing directory
-    dolphin "$(dirname "$1")"
-fi
